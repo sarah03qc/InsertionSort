87e9c9a6ffbc36b2a37089ed9da7fb92aaf42bbf
sarah03qc <prisilla2003@gmail.com>
2022-09-27 17:17:48 -0600

    mainp

--- mainp.cpp
+++ mainp.cpp
@@ -9,11 +9,12 @@ int main() {
 
     List<int>* listaint = new List<int>();
 
-    listaint->add(new int(66));
+    listaint->add(new int(32));
+    listaint->add(new int(5));
     listaint->add(new int(3));
-    listaint->add(new int(52));
+    listaint->add(new int(60));
+    listaint->add(new int(58));
     listaint->add(new int(1));
-    listaint->add(new int(25));
 
     InsertionSort ordenador;
 
@@ -23,6 +24,12 @@ int main() {
         int *currentint = listasorted->find(i);
         cout << *currentint << endl;
     }
-  
+
+    cout << ordenador.getComparisions() << " comparaciones." << endl;
+    cout << ordenador.getInterchanges() << " intercambios." << endl;
+
+   
+    cout << ordenador.getName() << endl;
+
     return 0;
 }
