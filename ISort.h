b187838346e12d4c4a7596166d892cc4125a4780
sarah03qc <prisilla2003@gmail.com>
2022-09-27 17:16:11 -0600

    ISort

--- ISort.h
+++ ISort.h
@@ -8,6 +8,9 @@ class ISort {
     public:
         // sort retorna la lista de enteros ya ordenada
         virtual List<int>* sort(List<int>* pNumberList) = 0;
+        virtual string getName() = 0;
+        virtual int getComparisions() = 0;
+        virtual int getInterchanges() = 0;
 };
 
 #endif
